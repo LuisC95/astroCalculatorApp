ddda1d57d0ac54c9712e41c684b5da7dd5a1f439
LuisC95 <luiscarmona.95@gmail.com>
2024-03-11 22:56:59 -0400

    issue solved Text label should not be in LoginForm

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginForm from './src/components/LoginForm';
 
-export default function App() 
-{
-  return 
-  (
+export default function App() {
+  return (
     <View style={styles.container}>
       <Text>hola mi amor</Text>
       <Text>Luis Carmona</Text>
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Button, TextInput, Text, View } from "react-native";
+import { Button, TextInput, View } from "react-native";
 
 export default function LoginForm() {
     return ( 
     <View>
-        <TextInput placeholder="Email"/>;
-        <TextInput placeholder="Contraseña"/>;
-        <Button title="Enviar" onPress={() => console.log("enviado")}/>
+        <TextInput placeholder="Email" /> 
+        <TextInput placeholder="Contraseña" /> 
+        <Button title="Enviar" onPress={() => console.log("enviado")} />  
     </View>
     );
 }
